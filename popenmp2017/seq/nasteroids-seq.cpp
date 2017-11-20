bae9a2f73bd2ed325778f18b0e72fde4fb6d5c43
charlestone <carlosdr2332@gmail.com>
2017-11-20 20:18:23 +0100

    argumentos
    comprobacion de argumentos completa

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -1,24 +1,40 @@
 /* Librerias */
 #include <iostream>
 #include <regex>
+#include <cmath>
 using namespace std;
 /* Funciones */
 bool checkArgs ( int argc, char *argv[]) {
-	regex r("\\d+");
+	regex e("\\d+");
+	regex f("\\d+\\.\\d+");
 	bool check = true;
+	/* Comprobacion de numero de argumentos*/
 	if(argc != 6) {
 		check = false;
 	} else {
-		if(!regex_match(argv[1], r)) {
+		/* Comprobacion de numero de asteroides */
+		if(!regex_match(argv[1], e)) {
 			check= false;
 		}
-		if(!regex_match(argv[2], r)) {
+		/* Comprobacion de numero de iteraciones*/
+		if(!regex_match(argv[2], e)) {
 			check= false;
 		}
-		if(!regex_match(argv[3], r)) {
+		/* Comprobacion de numero de planetas */
+		if(!regex_match(argv[3], e)) {
 			check= false;
 		}
-		if(regex_match(argv[5], r)) {
+		/* Comprobacion de posicion del rayo */
+		if(!regex_match(argv[4], f)) {
+			check= false;
+		} else {
+			double ray = stod(argv[4]);
+			if (ray > 199 || ray < 1) {
+				check = false;
+			}
+		}
+		/* Comprobacion de la semilla */
+		if(regex_match(argv[5], e)) {
 			int seed = stoi(argv[5]);
 			if(seed == 0){
 				check = false;
@@ -27,9 +43,8 @@ bool checkArgs ( int argc, char *argv[]) {
 			check = false;
 		}
 	}
-
 	if(!check) {
-		cerr << "Wrogn arguments.\nCorrect use:\nnasteroids-seq num_asteroides num_iteraciones num_planetas pos_rayo semilla\n";
+		cerr << "nasteroids-seq: Wrogn arguments.\nCorrect use:\nnasteroids-seq num_asteroides num_iteraciones num_planetas pos_rayo semilla\n";
 	}
 
 	return check;
@@ -37,6 +52,11 @@ bool checkArgs ( int argc, char *argv[]) {
 /* Main */
 int main(int argc, char *argv[])
 {
-	cout << checkArgs(argc, argv);
+	/* Llamamos a funcion para que compruebe argumentos */
+	if (!checkArgs(argc, argv)) {
+		return -1; 
+	}
+	/* Declaracion de variables*/
+	
 	return 0;
 }

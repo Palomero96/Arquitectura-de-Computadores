8de3c6df6cb65ec4ce91a83400ccdd76e4ef0ad1
Palomero96 <Palomero96@users.noreply.github.com>
2017-11-20 18:47:25 +0100

    Quitando errores

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -3,16 +3,27 @@
 #include <regex>
 using namespace std;
 /* Funciones */
-bool checkArgs ( int argc, char **argv[]) {
+bool checkArgs ( int argc, char *argv[]) {
 	regex r("\\d+");
-	smatch match;
 	bool check = true;
-	if(argc != 2) {
+	if(argc != 6) {
 		check = false;
 	} else {
-		if(regex_match(argv[1], match, r)) {
-			cout << match;
+		if(!regex_match(argv[1], r)) {
+			check= false;
 		}
+		if(!regex_match(argv[2], r)) {
+			check= false;
+		}
+		if(!regex_match(argv[3], r)) {
+			check= false;
+		}
+		if(!regex_match(argv[5], r)) {
+			int seed = stoi(argv[5]);
+			if(seed == 0){
+				check = false;
+			}
+		}		
 	}
 
 	if(!check) {
@@ -20,11 +31,10 @@ bool checkArgs ( int argc, char **argv[]) {
 	}
 
 	return check;
-
 }
 /* Main */
-int main(int argc, char const **argv[])
+int main(int argc, char *argv[])
 {
-	cout << checkArgs(argc, argv[]);
+	cout << checkArgs(argc, argv);
 	return 0;
 }

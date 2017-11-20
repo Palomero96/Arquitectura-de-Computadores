87a83e48c01fc8ba99dfd4e90e6fbd885ac95c94
asik03 <asier.alcaide@yahoo.es>
2017-11-20 22:47:01 +0100

    Constantes

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -2,6 +2,17 @@
 #include <iostream>
 #include <regex>
 #include <cmath>
+
+/* Constantes */
+const double gravity = 0.00006674; 
+const double t = 0.1; 		//Intervalo de tiempo
+const double dmin = 2.0; 	//Distancia minima
+const double width = 200; 	//Anchura del espacio			**No estoy seguro del tipo, confirmen
+const double height = 200;	//Altura del espacio 			**No estoy seguro del tipo, confirmen
+const int ray_width = 4;	//Anchura del rayo				**No estoy seguro del tipo, confirmen
+const int m = 1000;			//Media para el calculo de distribucion normal de las masas				**No estoy seguro del tipo, confirmen
+const int sdm = 50; 		//Desviacion estandar para el calculo de la dist normal de las masas 	**No estoy seguro del tipo, confirmen
+
 using namespace std;
 /* Funciones */
 bool checkArgs ( int argc, char *argv[]) {
@@ -14,19 +25,19 @@ bool checkArgs ( int argc, char *argv[]) {
 	} else {
 		/* Comprobacion de numero de asteroides */
 		if(!regex_match(argv[1], e)) {
-			check= false;
+			check = false;
 		}
 		/* Comprobacion de numero de iteraciones*/
 		if(!regex_match(argv[2], e)) {
-			check= false;
+			check = false;
 		}
 		/* Comprobacion de numero de planetas */
 		if(!regex_match(argv[3], e)) {
-			check= false;
+			check = false;
 		}
 		/* Comprobacion de posicion del rayo */
 		if(!regex_match(argv[4], f)) {
-			check= false;
+			check = false;
 		} else {
 			double ray = stod(argv[4]);
 			if (ray > 199 || ray < 1) {

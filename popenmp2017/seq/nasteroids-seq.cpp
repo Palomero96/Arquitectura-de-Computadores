739f0bb403b1fb9f9d2b9e1ac97031adbb899691
charlestone <carlosdr2332@gmail.com>
2017-11-20 12:13:51 +0100

    header en nasteroids
    añado el codigo que habia en el header al codigo principal

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -1,6 +1,29 @@
+/* Librerías */
 #include <iostream>
-#include "funciones.h"
-int main(int argc, char const *argv[])
+#include <regex>
+using namespace std;
+/* Funciones */
+bool checkArgs ( int argc, char **argv[]) {
+	regex r("\\d+");
+	smatch match;
+	bool check = true;
+	if(argc != 2) {
+		check = false;
+	} else {
+		if(regex_match(argv[1], match, r)) {
+			cout << match;
+		}
+	}
+
+	if(!check) {
+		cerr << "Wrogn arguments.\nCorrect use:\nnasteroids-seq num_asteroides num_iteraciones num_planetas pos_rayo semilla\n";
+	}
+
+	return check;
+
+}
+/* Main */
+int main(int argc, char const **argv[])
 {
 	cout << checkArgs(argc, argv[]);
 	return 0;

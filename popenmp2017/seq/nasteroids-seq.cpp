a45790599660bd7f1a456636064b6cd1934497ad
charlestone <carlosdr2332@gmail.com>
2017-11-20 23:28:53 +0100

    datos iniciales
    comienzo creacion de estructuras de datos iniciales

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -2,10 +2,28 @@
 #include <iostream>
 #include <regex>
 #include <cmath>
+#include <random>
+#include <forward_list>
 using namespace std;
+/* Estructuras de datos */
+struct asteroide {
+	int id;
+	double x;
+	double y;
+	double mass;
+	double vx;
+	double vy;
+	double px;
+	double py;
+	double pvx;
+	double pvy;
+};
 /* Funciones */
-bool checkArgs ( int argc, char *argv[]) {
+/* Metodo de comprobacion de variables */
+bool checkArgs (int argc, char *argv[]) {
+	/* Expresion regular para enteros */
 	regex e("\\d+");
+	/* Expresion regular para numeros en coma flotante */
 	regex f("\\d+\\.\\d+");
 	bool check = true;
 	/* Comprobacion de numero de argumentos*/
@@ -49,6 +67,23 @@ bool checkArgs ( int argc, char *argv[]) {
 
 	return check;
 }
+/* Metodo de creacion de asteroides */
+void createAsts (int num_asteroides, int semilla, forward_list<asteroide> &asteroides){
+	/* Constantes */
+	double mass = 1000.0;
+	double sdm = 50.0;
+	double width = 200.0;
+	double height = 200.0;
+	/* Distribuciones para coordenadas y masas */
+	default_random_engine re{semilla};
+	uniform_real_distribution<double> xdist{0.0, nextafter(width, numeric_limits<double>::max())};
+	uniform_real_distribution<double> ydist{0.0, nextafter(height, numeric_limits<double>::max())};
+	normal_distribution<double> mdist{mass, sdm};
+	/* Creacion de asteroides */
+	for(){
+	}	
+}
+
 /* Main */
 int main(int argc, char *argv[])
 {
@@ -56,7 +91,16 @@ int main(int argc, char *argv[])
 	if (!checkArgs(argc, argv)) {
 		return -1; 
 	}
-	/* Declaracion de variables*/
+	/* Declaracion de constantes */
 	
+	/* Declaracion de variables */
+	int num_asteroides = stoi(argv[1]);
+    int num_iteraciones = stoi(argv[2]);
+    int num_planetas = stoi(argv[3]);
+    double pos_rayo = stod(argv[4]);
+    int semilla = stoi(argv[5]);
+    forward_list<asteroide> asteroides;
+    createAsts(num_asteriodes, semilla, asteroides);
+    
 	return 0;
 }

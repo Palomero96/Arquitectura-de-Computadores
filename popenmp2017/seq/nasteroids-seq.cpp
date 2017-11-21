4d782ca98450a0e45e4b1048c4414391288fda0d
asik03 <asier.alcaide@yahoo.es>
2017-11-21 11:02:35 +0100

    Creacion de planetas y de asteroides

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -8,14 +8,32 @@
 const double gravity = 0.00006674; 
 const double t = 0.1; 		//Intervalo de tiempo
 const double dmin = 2.0; 	//Distancia minima
-const double width = 200; 	//Anchura del espacio			**No estoy seguro del tipo, confirmen
-const double height = 200;	//Altura del espacio 			**No estoy seguro del tipo, confirmen
+//const double width = 200; 	//Anchura del espacio			**No estoy seguro del tipo, confirmen
+//const double height = 200;	//Altura del espacio 			**No estoy seguro del tipo, confirmen
 const int ray_width = 4;	//Anchura del rayo				**No estoy seguro del tipo, confirmen
-const int m = 1000;			//Media para el calculo de distribucion normal de las masas				**No estoy seguro del tipo, confirmen
-const int sdm = 50; 		//Desviacion estandar para el calculo de la dist normal de las masas 	**No estoy seguro del tipo, confirmen
+//const int m = 1000;			//Media para el calculo de distribucion normal de las masas				
+//const int sdm = 50; 		//Desviacion estandar para el calculo de la dist normal de las masas 	
 
 using namespace std;
 
+/* Estructuras de datos */
+struct Asteroide {
+	double x;
+	double y;
+	double mass;
+	double vx;
+	double vy;
+	double px;
+	double py;
+	double pvx;
+	double pvy;
+};
+
+struct Planeta {
+	double x;
+	double y;
+	double mass;
+};
 /* Funciones */
 bool checkArgs ( int argc, char *argv[]) {
 	regex e("\\d+");
@@ -52,8 +70,12 @@ bool checkArgs ( int argc, char *argv[]) {
 			if(seed == 0){
 				check = false;
 			}
-		} else {
-			check = false;
+		}
+		/* Comprobacion maximo asteroides e iteraciones */
+		int num_asteroides = stoi(argv[1]);
+		int num_iteraciones = stoi(argv[2]);
+    	if(num_asteroides > 1000 || num_iteraciones >5000){
+    		check = false;
 		}
 	}
 	if(!check) {
@@ -62,22 +84,80 @@ bool checkArgs ( int argc, char *argv[]) {
 
 	return check;
 }
+
 /* Main */
 int main(int argc, char *argv[]){
-	
 	
 	/* Llamamos a funcion para que compruebe argumentos */
 	if (!checkArgs(argc, argv)) {
 		return -1; 
 	}
-	/* Declaracion de variables*/
-	int seed = stoi(argv[5]);
 	
-	/* Código previo necesario para la obtención de las posiciones iniciales de los asteroides */
+	/* Declaracion de variables*/
+	int num_asteroides = stoi(argv[1]);
+    //int num_iteraciones = stoi(argv[2]);
+    int num_planetas = stoi(argv[3]);
+    //double pos_rayo = stod(argv[4]);
+	int seed = stoi(argv[5]);
+	    
+	/* Codigo previo necesario para la obtencion de las posiciones iniciales de los asteroides */
 	default_random_engine re{seed};
-	uniform_real_distribution<double> xdist{0.0, std::nextafter(width, std :: numeric_limits<double>::max())};
-	uniform_real_distribution<double> ydist{0.0, std::nextafter(height, std :: numeric_limits<double>::max())};
-	normal_distribution<double> mdist{m, sdm};
+	uniform_real_distribution<double> xdist{0.0, std::nextafter(200, std :: numeric_limits<double>::max())};
+	uniform_real_distribution<double> ydist{0.0, std::nextafter(200, std :: numeric_limits<double>::max())};
+	normal_distribution<double> mdist{1000, 50}; //Media y desv estandar para el calculo de distrib normal de masas. Lo ponemos directamente para evitar la creacin de más variables
 
+	/* Creacion Asteroides */
+	Asteroide asteroides[num_asteroides-1]; //Creamos un array con los asteroides
+	for(int i = 1; i < num_asteroides; i++){
+		cerr << "Asteroide " << i << ": \n";
+		asteroides[i].x = xdist(re); //Asignamos a cada asteroide un valor aleatorio de la posicion de x
+		asteroides[i].y = ydist(re); //Lo mismo con la y
+		asteroides[i].mass = mdist(re); //Lo mismo con la masa
+		cerr << asteroides[i].x << " " << asteroides[i].y << " " << asteroides[i].mass << "\n";	
+	}
+	
+	/*Creacion Planetas*/
+	Planeta planetas[num_planetas-1]; //Creamos un array con los planetas
+	int j = 1;
+	while(1){  //Con este bucle asignaremos la masa y la posicion de cada planeta. Si el contador supera al numero de planetas, salimos del bucle
+
+		planetas[j].x = 0.0;
+		planetas[j].y = ydist(re);
+		planetas[j].mass = 10 * mdist(re); //Multiplicamos por 10 la masa de un asteroide para obtener la del planeta
+		j++;
+		
+		if(j > num_planetas){
+			break;
+		}
+		planetas[j].x = xdist(re);
+		planetas[j].y = 200;
+		planetas[j].mass = 10 * mdist(re);
+		j++;
+		
+		if(j > num_planetas){
+			break;
+		}
+		planetas[j].x = 200;
+		planetas[j].y = ydist(re);
+		planetas[j].mass = 10 * mdist(re);
+		j++;
+			
+		if(j > num_planetas){
+			break;
+		}		
+		planetas[j].x = xdist(re);
+		planetas[j].y = 0;
+		planetas[j].mass = 10 * mdist(re);
+		j++;
+		
+		if(j > num_planetas){
+			break;
+		}
+	}
+
+	for(int i = 1; i < num_planetas; i++){
+		cerr << "Planeta " << i << ": \n";
+		cerr << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << "\n";	
+	}
 	return 0;
 }

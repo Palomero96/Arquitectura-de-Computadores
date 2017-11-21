ae2576759c302f489cf17bc7d0cebb2a41ffac6c
charlestone <carlosdr2332@gmail.com>
2017-11-21 22:33:26 +0100

    datos iniciales
    Está implementada la creación e impresión de datos iniciales.

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -4,6 +4,8 @@
 #include <cmath>
 #include <random>
 #include <forward_list>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 /* Estructuras de datos */
 struct asteroide {
@@ -18,6 +20,12 @@ struct asteroide {
 	double pvx;
 	double pvy;
 };
+struct planeta {
+	double x;
+	double y;
+	double mass;
+};
+
 /* Funciones */
 /* Metodo de comprobacion de variables */
 bool checkArgs (int argc, char *argv[]) {
@@ -68,19 +76,63 @@ bool checkArgs (int argc, char *argv[]) {
 	return check;
 }
 /* Metodo de creacion de asteroides */
-void createAsts (int num_asteroides, int semilla, forward_list<asteroide> &asteroides){
-	/* Constantes */
+void createAstros (int num_asteroides, int num_planetas, unsigned int semilla, forward_list<asteroide> &asteroides, planeta *planetas){
+	/* Constantes y variables */
 	double mass = 1000.0;
 	double sdm = 50.0;
 	double width = 200.0;
 	double height = 200.0;
+	int i = 0;
 	/* Distribuciones para coordenadas y masas */
 	default_random_engine re{semilla};
 	uniform_real_distribution<double> xdist{0.0, nextafter(width, numeric_limits<double>::max())};
 	uniform_real_distribution<double> ydist{0.0, nextafter(height, numeric_limits<double>::max())};
 	normal_distribution<double> mdist{mass, sdm};
 	/* Creacion de asteroides */
-	for(){
+	for( i = 0 ; i < num_asteroides ; i++){
+		/* Creamos el asteroide y rellenamos sus campos */
+		asteroide aaux;
+		aaux.id = i;
+		aaux.x = xdist(re);
+		aaux.y = ydist(re);
+		aaux.mass = mdist(re);
+		aaux.vx = 0.0;
+		aaux.vy = 0.0;
+		aaux.px = 0.0;
+		aaux.py = 0.0;
+		aaux.pvx = 0.0;
+		aaux.pvy = 0.0;
+		/* Añadimos el asteroide a la lista */
+		asteroides.push_front(aaux);
+	}
+	/* Creacion de planetas*/
+	for(i = 0; i < num_planetas ; i++){
+		/* Creamos el planeta */
+		planeta paux;
+		/* Rellenamos sus ejes en funcion de su resto con 4 */	
+		switch ( i%4 ){
+    		case 0:
+    			paux.x = 0.0;
+    			paux.y = ydist(re);
+    			break;
+    		case 1:
+    			paux.x = xdist(re);
+    			paux.y = 0.0;
+				break;
+    		case 2:
+    			paux.x = 200.0;
+    			paux.y = ydist(re);
+				break;
+    		case 3:
+    			paux.x = xdist(re);
+    			paux.y = 200.0;
+    			break;
+		}
+		/* Rellenamos su masa */
+		paux.mass = mdist(re)*10;
+		/* Añadimos el planeta al array */
+		planetas[i] = paux;
+		
 	}	
 }
 
@@ -99,9 +151,27 @@ int main(int argc, char *argv[]){
     int num_iteraciones = stoi(argv[2]);
     int num_planetas = stoi(argv[3]);
     double pos_rayo = stod(argv[4]);
-    int semilla = stoi(argv[5]);
+    unsigned int semilla = stoi(argv[5]);
+    /* Creamos lista para los asteroides */
     forward_list<asteroide> asteroides;
-    createAsts(num_asteriodes, semilla, asteroides);
-    
+    /* Creamos array para planetas */
+	planeta *planetas = new planeta[num_planetas];
+	/* Llamamos a la funcion para crear los astros */
+    createAstros(num_asteroides, num_planetas, semilla, asteroides, planetas);
+    ofstream init("init_conf.txt");
+    /* Imprimimos los datos iniciales en el fichero */
+    init << num_asteroides << " " << num_iteraciones << " " << num_planetas << " " << pos_rayo << " " << semilla << endl;
+    /* Damos la vuelta a la lista */
+    asteroides.reverse();
+    /* Imprimimos los asteroides en el fichero */
+    for (auto itri = asteroides.begin(); itri != asteroides.end(); ++itri ){
+    	init << fixed << setprecision(3) <<  itri->x << " " << itri->y << " " << itri->mass << endl;
+    }
+    /* Imprimimos los planetas en el fichero */
+    for (int i = 0; i<num_planetas;i++) {
+    	init << fixed << setprecision(3) << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << " " << endl;
+	}
+	/* Imprimimos el rayo */
+	init << fixed << setprecision(3) << 0.000 << " " << pos_rayo << endl;
 	return 0;
 }

103a0dbdbbba99020a5b88ecd9c226b3d4399f94
Palomero96 <Palomero96@users.noreply.github.com>
2017-11-26 19:13:17 +0100

    okoko

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -75,27 +75,66 @@ int main(){
     uniform_real_distribution <double> xdist{0.0, nextafter(width, numeric_limits<double>::max())};
     uniform_real_distribution <double> ydist{0.0, nextafter(height, numeric_limits<double>::max())};
     normal_distribution <double> mdist{mass, sdm};
-  	vector <planeta> planetas;
+  	vector <planeta> planetas (4);
+  	//planeta *planetas = new planeta[4];
     	//#pragma omp parallel num_threads(4) shared(planetas){
 		
 		double ini = omp_get_wtime();
 		for(int i=0; i<4; i++){
-		planeta nombre;
-		nombre.id=i;
-		nombre.x=xdist(re);
+		
+		planetas[i].id=i;
+	/*	nombre.x=xdist(re);
 		nombre.y=ydist(re);
 		nombre.mass=mdist(re); 
-		planetas.push_back(nombre);
+		planetas.push_back(nombre);*/
 		
 		}
 		double fin = omp_get_wtime();
 		cout << fin - ini << endl;
 //	}
-		for(unsigned i=0; i<4; i++){
+	/*	for(int i=0; i<4; i++){
 		
 		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;
 		
 		}
+	delete[] planetas;
+	
+cout << "PENE" << planetas[0].id << endl;*/
+	for(unsigned i=0; i<4; i++){
+		
+		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;
+		
+		}
+		cout << "3. capacity of myvector: " << planetas.capacity() << '\n';
+
+	for(unsigned i=0; i<planetas.size(); i++){
+		if(i==2){
+		planetas.erase(planetas.begin()+i);
+		delete &planetas[i];
+			i--;
+		}
+		
+	}
+
+	planetas.shrink_to_fit();
+	
+	
+ cout << "3. capacity of myvector: " << planetas.capacity() << '\n';
+	for(unsigned i=0; i<planetas.size(); i++){
+		
+		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;
+		
+		}
+	for(unsigned i=0; i<planetas.size(); i++){
+		if(i==0){
+			planetas.erase(planetas.begin()+i);
+		i--;
+		}
+	
+	}
+	
+	planetas.shrink_to_fit();
+cout << "3. capacity of myvector: " << planetas.capacity() << '\n';
 	
 
 	

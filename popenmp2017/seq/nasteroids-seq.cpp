a18472bf49d1c0cdb3cbef4edcfd54159e78318a
Palomero96 <Palomero96@users.noreply.github.com>
2017-11-27 13:13:47 +0100

    novato

--- popenmp2017/seq/nasteroids-seq.cpp
+++ popenmp2017/seq/nasteroids-seq.cpp
@@ -7,40 +7,7 @@
 #include <forward_list>
 #include <omp.h>
 using namespace std;
-/* Funciones */
-/*bool checkArgs ( int argc, char *argv[]) {
-	regex r("\\d+");
-	regex f("[+-]?([0-9]*[.])?[0-9]+");
-	bool check = true;
-	if(argc != 6) {
-		check = false;
-	} else {
-		if(!regex_match(argv[1], r)) {
-			check= false;
-		}
-		if(!regex_match(argv[2], r)) {
-			check= false;
-		}
-		if(!regex_match(argv[3], r)) {
-			check= false;
-		}
 
-		if(regex_match(argv[5], r)) {
-			int seed = stoi(argv[5]);
-			if(seed == 0){
-				check = false;
-			}
-		}else {
-			check = false;
-		}		
-	}
-	
-	if(!check) {
-		cerr << "Wrong arguments.\nCorrect use:\nnasteroids-seq num_asteroides num_iteraciones num_planetas pos_rayo semilla\n";
-	}
-
-	return check;
-}*/
 struct planeta {
 	int id;
 	double x;
@@ -49,22 +16,6 @@ struct planeta {
 };
 /* Main */
 int main(){
-/*		if(argc != 6) {
-		return -1;
-	}
-	//Definimos las variables
-	int num_asteroides = stoi(argv[1]);
-	int num_iteraciones = stoi(argv[2]);
-	int num_planetas = stoi(argv[3]);
-	double pos_rayo = stod(argv[4]);
-	int semilla = stoi(argv[5]);
-	//Calculamos los parametros iniciales
-	
-	
-	//Creamos el archivo donde escribiremos los parametros de entrada y las 
-	ofstream out("init_conf.txt");
-	out << fixed << setprecision(3) << num_asteroides <<" "<<num_iteraciones<<" "<<num_planetas<<" "<< pos_rayo<< " " <<semilla<< endl;
-*/
 	
 	double mass = 1000.0;
     double sdm = 50.0;
@@ -75,32 +26,35 @@ int main(){
     uniform_real_distribution <double> xdist{0.0, nextafter(width, numeric_limits<double>::max())};
     uniform_real_distribution <double> ydist{0.0, nextafter(height, numeric_limits<double>::max())};
     normal_distribution <double> mdist{mass, sdm};
-  	vector <planeta> planetas (4);
+  	vector <planeta> planetas (6);
   	//planeta *planetas = new planeta[4];
     	//#pragma omp parallel num_threads(4) shared(planetas){
 		
-		double ini = omp_get_wtime();
-		for(int i=0; i<4; i++){
+	
+		for(int i=0; i<6; i++){
 		
 		planetas[i].id=i;
-	/*	nombre.x=xdist(re);
-		nombre.y=ydist(re);
-		nombre.mass=mdist(re); 
-		planetas.push_back(nombre);*/
+		planetas[i].x=i;
+		planetas[i].y=i;
+		planetas[i].mass=i; 
 		
 		}
-		double fin = omp_get_wtime();
-		cout << fin - ini << endl;
+		for(int i=0; i<6; i++){
+		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;		
+		}
 //	}
-	/*	for(int i=0; i<4; i++){
-		
-		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;
-		
+		for(int i=0; i<20; i++){
+			for(int j=0; j<4; j++){
+				planetas[j].x=planetas[j].x + planetas[j+1].x;
+			}
 		}
-	delete[] planetas;
+			for(int i=0; i<6; i++){
+		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;		
+		}
 	
-cout << "PENE" << planetas[0].id << endl;*/
-	for(unsigned i=0; i<4; i++){
+	
+
+/*	for(unsigned i=0; i<4; i++){
 		
 		cout << planetas[i].id << " " << planetas[i].x << " " << planetas[i].y << " " << planetas[i].mass << endl;
 		
@@ -136,7 +90,7 @@ cout << "PENE" << planetas[0].id << endl;*/
 	planetas.shrink_to_fit();
 cout << "3. capacity of myvector: " << planetas.capacity() << '\n';
 	
-
+*/
 	
 	return 0;
 	
